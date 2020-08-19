7f5c10cad8896af8b1b51861aab2bfe496325a8d
Thouhedul Islam <thouhedul@cartx.io>
2020-08-20 01:02:54 +0600

    Complete 20 August Class

--- resources/views/tasks/partials/create-form.blade.php
+++ resources/views/tasks/partials/create-form.blade.php
@@ -1,15 +1,21 @@
-<form action="/tasks/store" method="POST">
-	@csrf
+<div class="card">
+	<div class="card-body bg-light">
+		<form action="/tasks/store" method="POST">
+			@csrf
 
-	<div>
-		<input type="text" name="title" placeholder="Type your title" required="required">
-	</div>
-	<br>
-	<div>
-		<textarea name="description" id="" cols="30" rows="3" placeholder="Type optional description."></textarea>
-	</div>
-	<br>
-	<div>
-		<button type="submit">Add new task</button>
-	</div>
-</form>
+			<div class="form-group">
+				<label for="exampleInputEmail1">Title</label>
+				<input type="text" class="form-control" name="title" placeholder="Type your title" required="required">
+			</div>
+			<br>
+			<div>
+				<label for="exampleInputEmail1">Description</label>
+				<textarea name="description" class="form-control" cols="30" rows="3" placeholder="Type optional description."></textarea>
+			</div>
+			<br>
+			<div>
+				<button class="btn btn-primary" type="submit">Add new task</button>
+			</div>
+		</form>
+	</div>	
+</div>
